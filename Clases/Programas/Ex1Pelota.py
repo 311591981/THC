83c699b209c9d08d4b868828d22c1c9eb8cd6935
311591981 <els1993@ciencias.unam.mx>
2019-01-11 15:46:47 -0600

    Tarea 1 modificada + programas de la clase del viernes 11/01

--- Clases/Programas/Ex1Pelota.py
+++ Clases/Programas/Ex1Pelota.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
 #Emilio López Sotelo
+
 #y = (v0 * t) - (1.0/2 * g * t ^ 2)
+
+'''
+Información del problema resuelto en este archivo
+'''
 
 print (34 * 3) - (1/2 * 9.81 * 3 ** 2) #t=3 #1/2 Calcula la división entera
 print
