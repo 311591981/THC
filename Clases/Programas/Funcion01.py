da8f65c336dd0ce96f98408ac9a59399c960938b
311591981 <els1993@ciencias.unam.mx>
2019-01-15 14:03:20 -0600

    Ejercicio en clase

--- Clases/Programas/Funcion01.py
+++ Clases/Programas/Funcion01.py
@@ -3,3 +3,28 @@ def vAbsoluto(x):
         return(x)
     else:
         return(-x)
+
+def raiz(x):
+    h=x
+    b=1.0
+    e=0.0000000001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+    return (h)
+
+print raiz(1)
+print raiz(4)
+print raiz(9)
+print raiz(9.1)
+print raiz(1000000)
+
+def raiz1(x):
+    h=x
+    b=1.0
+    e=0.0000000001
+    while vAbsoluto(b-h)>e:
+        h = (b+h)/2
+        b = x/h
+        print "Hola"
+    return (h)
