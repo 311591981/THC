bc47840f43e2139d50eb8bbd16a6915355f46870
311591981 <els1993@ciencias.unam.mx>
2019-01-14 23:00:32 -0600

    Tarea 3 y modificaciones a los resumenes

--- Clases/Programas/Diana.py
+++ Clases/Programas/Diana.py
@@ -1,4 +1,4 @@
-def valorcoord(x,y):
+def valorcoord1(x,y):
     if x<5 and y<10:
         return(3)
     else:
@@ -28,32 +28,29 @@ def valorcoord(x,y):
                                     else:
                                         print "ERROR"
 
-def valorcoord2(x,y):
-    if x<5 or x>25 and y<10 or y>30:
+def valorcoord1_2(x,y):
+    if (x<5 or x>25) and (y<10 or y>30):
         return(3)
     else:
-        if x<5 or x>25 and 10<=y<=30:
+        if (x<5 or x>25) and (10<=y<=30):
             return(5)
         else:
-            if 5<=x<=25 and y<10 or y>30:
+            if (5<=x<=25) and (y<10 or y>30):
                 return(7)
             else:
-                if 5<=x<=25 and 10<=y<=30:
+                if (5<=x<=25) and (10<=y<=30):
                     return(10)
                 else:
                     print "ERROR"
-def valorcoord3(x,y):
-    if x<5 or x>25 and y<10 or y>30:
+                    
+def valorcoord1_3(x,y):
+    if (x<5 or x>25) and (y<10 or y>30):
         return(3)
-    elif x<5 or x>25 and 10<=y<=30:
-            return(5)
-    elif 5<=x<=25 and y<10 or y>30:
-                return(7)
-    elif 5<=x<=25 and 10<=y<=30:
-                    return(10)
+    elif (x<5 or x>25) and (10<=y<=30):
+        return(5)
+    elif (5<=x<=25) and (y<10 or y>30):
+        return(7)
+    elif (5<=x<=25) and (10<=y<=30):
+        return(10)
     else:
         print "ERROR"
-
-#circulo con centro en p=(15,20) y radio r=(10)
-
-                                
